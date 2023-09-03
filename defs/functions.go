cfc8dee3f8180ef035161769d78f31cb6380fd78
kvo <kvo@zotu.dev>
2023-09-03 18:52:00 +0930

    Fix old refernces to old "std" name of package

--- defs/functions.go
+++ defs/functions.go
@@ -1,11 +1,4 @@
-// Package std collects implements an alternative standard library.
-//
-// While Go's standard library and built-in identifiers typically do well in
-// catering to the needs of the programmer, some functions that could assist
-// the programmer in general programming cases do not exist in the standard Go
-// distribution. Package std attempts to resolve this issue by providing a set
-// of useful identifiers for general programming.
-package std
+package defs
 
 import (
 	"fmt"
@@ -13,13 +6,13 @@ import (
 	"git.sr.ht/~kvo/libgo/errors"
 )
 
-// Access returns the nth element of slice s. Returns error if slice s does not
+// Get returns the nth element of slice s. Returns error if slice s does not
 // have an element at index n.
 //
 // For most access attempts on strings, []rune(str) will be a more appropriate
 // choice than []byte(str) for the parameter s, as no individual byte in
 // []byte(str) is guaranteed to hold a single Unicode code point.
-func Access[T any](s []T, n int) (T, errors.Error) {
+func Get[T any](s []T, n int) (T, errors.Error) {
 	var none T
 	if n > len(s)-1 || n < 0 {
 		return none, errors.New(
@@ -31,7 +24,7 @@ func Access[T any](s []T, n int) (T, errors.Error) {
 }
 
 // Contains checks slice s for the existence of an element elem.
-func Contains[T comparable](s []T, elem T) bool {
+func Has[T comparable](s []T, elem T) bool {
 	for _, v := range s {
 		if v == elem {
 			return true
---
+++ doc.go
@@ -0,0 +1,7 @@
+// Package libgo collects implements an alternative standard library.
+//
+// While Go's standard library and built-in identifiers typically do well in
+// catering to the needs of the programmer, some functions that could assist
+// the programmer in general programming cases do not exist in the standard Go
+// distribution. Package libgo attempts to resolve this issue by providing a set
+// of useful identifiers for general programming.
