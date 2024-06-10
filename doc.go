cdeaa7d15e1e3ad7dbb73bd92c866ef2ba13e109
kvo <kvo@zotu.dev>
2024-06-10 15:45:52 +0930

    doc.go: fix package name in documentation
    Signed-off-by: kvo <kvo@envs.net>

--- doc.go
+++ doc.go
@@ -1,8 +1,8 @@
-// Package go-std collects implements an alternative standard library.
+// Package std collects implements an alternative standard library.
 //
 // While Go's standard library and built-in identifiers typically do well in
 // catering to the needs of the programmer, some functions that could assist
 // the programmer in general programming cases do not exist in the standard Go
-// distribution. Package go-std attempts to resolve this issue by providing a
-// set of useful identifiers for general programming.
+// distribution. Package std attempts to resolve this issue by providing a set
+// of useful identifiers for general programming.
 package std
